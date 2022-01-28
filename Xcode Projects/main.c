b818266778985a7d3818705e0dcab9c90b4325ac
Rupaj Sen <rupajps4@gmail.com>
2022-01-28 11:17:23 +0530

    Initial Commit

---
+++ Xcode Projects/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Xcode Projects
+//
+//  Created by Rupaj Sen on 28/01/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
