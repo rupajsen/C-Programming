3de7061e88e6b32b1f6cad900f59df2842ea81a4
Rupaj Sen <rupajps4@gmail.com>
2022-02-02 23:25:19 +0530

    logical operators

---
+++ logical operator/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  logical operator
+//
+//  Created by Rupaj Sen on 02/02/22.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    _Bool a = 1;
+    _Bool b = 1;
+    _Bool result;
+    
+    result = a || b;
+    
+    printf("%d\n",result);
+    return 0;
+}
