402cf8e21e48e7b832e5eddb5f544b9cadeaa924
Rupaj Sen <rupajps4@gmail.com>
2022-02-06 20:21:08 +0530

    added bitwise progm.

---
+++ Bitwise operators/main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  Bitwise operators
+//
+//  Created by Rupaj Sen on 06/02/22.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    unsigned int a = 60;     // 0011 1100
+    unsigned int b = 13;     // 0000 1101
+    int result = 0;
+    
+    result = a & b ;
+    
+    // 0000 1100
+    
+    printf("Result is : %d\n",result);
+
+    return 0;
+}
