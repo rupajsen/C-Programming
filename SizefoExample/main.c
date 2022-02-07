bd90c7a48df5841d2e7a8855e0da925b6bf1116e
Rupaj Sen <rupajps4@gmail.com>
2022-02-07 18:20:24 +0530

    size of examples

---
+++ SizefoExample/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  Print the byte size of the basic data types
+//
+//  Created by Rupaj Sen on 07/02/22.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Variables of type char occupy %zd bytes\n", sizeof(char));
+    printf("Variables of type short occupy %zd bytes\n", sizeof(short));
+    printf("Variables of type int occupy %zd bytes\n", sizeof(int));
+    printf("Variables of type long occupy %zd bytes\n", sizeof(long));
+    printf("Variables of type long long occupy %zd bytes\n", sizeof(long long));
+    printf("Variables of type float occupy %zd bytes\n", sizeof(float));
+    printf("Variables of type double occupy %zd bytes\n", sizeof(double));
+    printf("Variables of type long double occupy %zd bytes\n", sizeof(long double));
+    
+    return 0;
+}
