a69d192fb5129db7ff9220468b7a86d6c1c4232f
Rupaj Sen <rupajps4@gmail.com>
2022-02-24 20:28:53 +0530

    for loop

---
+++ For loop/main.c
@@ -0,0 +1,26 @@
+//
+//  main.c
+//  For loop
+//
+//  Created by Rupaj Sen on 24/02/22.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    unsigned long long sum = 0LL; //Stores the sum of the integers
+    unsigned int count = 0; //The number of integers to be summed
+    
+    //Read the number of integers to be summed
+    printf("\nEnter the number of integers you want to sum: ");
+    scanf(" %u", &count);
+    
+    //Sum integers from 1 to count
+    for(unsigned int i = 1 ; i <= count ; ++i)
+        sum +=i;
+    
+    printf("\nTotal of the first %u numbers is %llu\n",count ,sum);
+    
+    return 0;
+}
